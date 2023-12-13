4addcf37d377ad3edc7d95aae9737ad33f1d9785
Aravind A <118422089+AARAV26102004@users.noreply.github.com>
2023-12-13 14:50:12 +0530

    Add files via upload

---
+++ ONLINE-MEETING/DAY-1-LOGIN-SIGNUP/Admin.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Admin()
+{
+    return(
+        <div>
+        <center><h1>HAII USER YOU ARE LOGGED IN</h1></center>
+        </div>
+    );
+}
+export default Admin;
